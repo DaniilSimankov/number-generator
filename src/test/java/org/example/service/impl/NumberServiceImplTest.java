8b3267ce8b4f0eab2877c7da1e530151e8040d0e
Daniel <thedanils63@mail.ru>
2024-03-10 14:20:41 +0300

    added tests

--- src/test/java/org/example/service/impl/NumberServiceImplTest.java
+++ src/test/java/org/example/service/impl/NumberServiceImplTest.java
@@ -1,14 +1,30 @@
 package org.example.service.impl;
 
+import org.example.service.impl.NumberServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class NumberServiceImplTest {
 
-    @org.junit.jupiter.api.Test
+    @Mock
+    NumberServiceImpl numberService;
+
+    @Test
     void generateNextNumber() {
+        when(numberService.generateNextNumber()).thenReturn("A000AA 116 RUS");
+        String result = numberService.generateNextNumber();
+        assertEquals("A000AA 116 RUS", result);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void generateRandomNumber() {
+        when(numberService.generateRandomNumber()).thenReturn("C999BA 116 RUS");
+        String result = numberService.generateRandomNumber();
+        assertEquals("C999BA 116 RUS", result);
     }
 }
